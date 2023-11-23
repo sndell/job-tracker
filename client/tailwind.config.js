e7c2197585401e1131d09f55361a8072c458baf7
Benjamin <dev.besu@gmail.com>
2023-11-23 15:23:57 +0100

    feat: setup nuxt project & add tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        background: '#E7E7E7',
+        primary: '#FFFFFF',
+        secondary: '#CECECE',
+      },
+      textColor: {
+        primary: '#433F3F',
+        primaryLight: '#686767',
+        primaryDark: '#222222',
+      },
+      fontFamily: {
+        workSans: "'Work Sans', sans-serif;",
+      },
+    },
+  },
+  plugins: [],
+};
