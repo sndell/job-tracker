0389fa1e50045fa40a5ea9fb825bb552a0896e4f
sndell <main.besu@gmail.com>
2023-12-22 21:56:06 +0100

    feat (frontend): update gql host url

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   ],
   runtimeConfig: {
     public: {
-      GQL_HOST: 'https://fullstack-vue-server.up.railway.app',
+      GQL_HOST: 'https://server.vafan.store',
     },
   },
 });
